2cc9857ce514be30aadcaa1d9b1e10a7449f9c42
Top1055 <123alexfeetham@gmail.com>
2023-03-10 12:36:15 +0000

    change for revert

--- src/boot.rs
+++ src/boot.rs
@@ -1,7 +1,13 @@
 use crate::{print, println};
+<<<<<<< HEAD
 use crate::keyboard;
 
 pub extern fn init() {
     print!("\n\nWelcome to ScarabOS\n\n> ");
     keyboard::keyboard_loop();
+=======
+
+pub extern fn init() {
+    println!("Hello");
+>>>>>>> parent of 8b44150 (echo and keyboard works!)
 }
