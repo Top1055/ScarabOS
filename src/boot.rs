4b7a52b84f7d44c5643eac6da168fc141fe76882
Top1055 <123alexfeetham@gmail.com>
2023-03-10 12:37:36 +0000

    Revert "echo and keyboard works!"
    This reverts commit 8b4415025a4ae9c3ec961298c435d4469a44743f.

--- src/boot.rs
+++ src/boot.rs
@@ -1,10 +1,16 @@
 use crate::{print, println};
 <<<<<<< HEAD
+<<<<<<< HEAD
 use crate::keyboard;
 
 pub extern fn init() {
     print!("\n\nWelcome to ScarabOS\n\n> ");
     keyboard::keyboard_loop();
+=======
+
+pub extern fn init() {
+    println!("Hello");
+>>>>>>> parent of 8b44150 (echo and keyboard works!)
 =======
 
 pub extern fn init() {
